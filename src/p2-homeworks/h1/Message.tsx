f69129190b455e32aa6ee4147ffb9e524f0baa38
pavel5284 <pavel5284@mail.ru>
2022-03-03 20:14:48 +0200

    monday-1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {messageDataPropsType} from "./HW1";
+
+function Message(props: messageDataPropsType) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
